ac35c205ac23863c022eb65c3c8a727bed31784e
Meenakshi <meenakshi.e@atop.com.tw>
2023-05-12 12:27:03 +0530

    added test files

---
+++ modules.js
@@ -0,0 +1,16 @@
+// console.log(arguments);
+// console.log(require('module').wrapper);
+
+const C=require('./test-module-1')
+
+const calc1=new C()
+console.log(calc1.add(2,5));
+
+const {add,multiply,divide}=require('./test-module2')
+console.log(multiply(6,3));
+
+//caching
+
+require('./test-module-3')()
+require('./test-module-3')();
+require('./test-module-3')();
