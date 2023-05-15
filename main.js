66fea356b6e2753b019eda685c824ee81622eb33
Meenakshi <meenakshi.e@atop.com.tw>
2023-05-15 18:05:01 +0530

    basics of electron

---
+++ main.js
@@ -0,0 +1,45 @@
+/*
+const{BrowserWindow,app}=require('electron')
+const path=require('path')
+
+const createWindow=()=>{
+    const win=new BrowserWindow({
+        width:800,
+        height:600,
+        webPreferences:{
+            preload:path.join(__dirname,'preload.js')
+        }
+    })
+   win.loadFile('index.html')
+}
+ app.whenReady().then(() => {
+		createWindow();
+ });
+
+ app.on('window-all-closed',()=>{
+    if(process.platform!=='darwin') app.quit()
+ })
+*/
+const { app, BrowserWindow, ipcMain ,Menu} = require('electron');
+const path = require('path');
+ipcMain.on("msg",(event,data)=>{
+    console.warn(data)
+    event.reply("reply","thank you")
+})
+
+const createWindow = () => {
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+        title:"awesome app",
+		webPreferences: {
+            nodeIntegration:true
+			// preload: path.join(__dirname, 'preload.js'),
+		},
+	});
+	
+	win.loadFile('index.html');
+    win.webContents.openDevTools()
+};
+app.whenReady().then(createWindow);
+
